909c89a6f9c4212495fe255a4b483dc719e74827
raheemamer <raheem.amer22@gmail.com>
2022-10-01 12:37:45 +0200

    [Bugs] enhanced collision detection algorithm
    1. Fixed ball and paddle collision detection system 2.Made code look more cleaner

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Ping-Pong-Js-Game
+
 A ping pong game built using canvas and JS
+
+## Todos
+
+1. Fix styling
+2. Fix restart button
+3. Add score
+4. Make interactive ui for both players during collision.
+5. Create leaderboard
+6. Make Guest and user system
+7. Save score of each user
+8. Use something with realtime or socket.io as an experiment
--- main.js
+++ main.js
@@ -5,58 +5,100 @@ canvas.height = innerHeight;
 
 class Paddle {
     constructor({ position }) {
-        this.position = position;
+        this.position = position
         this.velocity = {
             x: 0,
-            y: 0
+            y: 0,
         }
-        this.width = 10;
-        this.height = 100;
+        this.width = 10
+        this.height = 100
     }
+
     draw() {
-        c.fillStyle = "white";
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        c.fillStyle = 'black'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
+
     update() {
-        this.draw();
-        if (this.position.y + this.velocity.y > 0 && this.position.y + this.height + this.velocity.y < canvas.height)
-            this.position.y += this.velocity.y;
+        this.draw()
+
+        if (
+            this.position.y + this.velocity.y > 0 &&
+            this.position.y + this.height + this.velocity.y < canvas.height
+        )
+            this.position.y += this.velocity.y
     }
 }
 
 class Ball {
     constructor({ position }) {
+        this.position = position
+
+        const speed = 2
         const direction = {
-            x: Math.random() - 0.5 >= 0 ? -1 : 1,
-            y: Math.random() - 0.5 >= 0 ? -1 : 1,
+            x: Math.random() - 0.5 >= 0 ? -speed : speed,
+            y: Math.random() - 0.5 >= 0 ? -speed : speed,
         }
-        this.position = position;
         this.velocity = {
             x: direction.x,
             y: direction.y,
         }
-        this.width = 10;
-        this.height = 10;
+
+        this.width = 10
+        this.height = 10
     }
+
     draw() {
-        c.fillStyle = 'white';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        c.fillStyle = 'black'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
+
     update() {
-        this.draw();
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
+        this.draw()
+        const rightSide = this.position.x + this.width + this.velocity.x
+        const leftSide = this.position.x + this.velocity.x
+        const bottomSide = this.position.y + this.height
+        const topSide = this.position.y
+
+        // paddle 1 collision
+        if (
+            leftSide <= paddle1.position.x + paddle1.width &&
+            bottomSide >= paddle1.position.y &&
+            topSide <= paddle1.position.y + paddle1.height
+        ) {
+            this.velocity.x = -this.velocity.x
+        }
+
+        // paddle 2 collision
+        if (
+            rightSide >= paddle2.position.x &&
+            bottomSide >= paddle2.position.y &&
+            topSide <= paddle2.position.y + paddle2.height
+        ) {
+            this.velocity.x = -this.velocity.x
+        }
+
+        // reverse y directions
+        if (
+            this.position.y + this.height + this.velocity.y >= canvas.height ||
+            this.position.y + this.velocity.y <= 0
+        ) {
+            this.velocity.y = -this.velocity.y
+        }
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
     }
 }
 
-const Paddle1 = new Paddle({
+const paddle1 = new Paddle({
     position: {
         x: 10,
         y: 100,
     },
 })
 
-const Paddle2 = new Paddle({
+const paddle2 = new Paddle({
     position: {
         x: canvas.width - 10 * 2,
         y: 100,
@@ -72,46 +114,34 @@ const ball = new Ball({
 
 function animate() {
     requestAnimationFrame(animate);
-    c.fillStyle = "black";
+    c.fillStyle = "white";
     c.fillRect(0, 0, canvas.width, canvas.height);
-    Paddle1.update();
-    Paddle2.update();
+    paddle1.update();
+    paddle2.update();
     ball.update()
 }
 animate();
 
 addEventListener('keydown', (event) => {
-    const speed = 3;
+    const speed = 6;
     switch (event.key) {
         // Player One key configurations
         case "w":
             // go up
-            Paddle1.velocity.y = -speed;
+            paddle1.velocity.y = -speed;
             break;
         case "s":
             // go down
-            Paddle1.velocity.y = speed;
-            break;
-        case "a":
-            // go left
-            break;
-        case "d":
-            // go right
+            paddle1.velocity.y = speed;
             break;
         // Player two key configurations
         case "ArrowUp":
             // go up
-            Paddle2.velocity.y = -speed;
+            paddle2.velocity.y = -speed;
             break;
         case "ArrowDown":
             // go down
-            Paddle2.velocity.y = speed;
-            break;
-        case "ArrowLeft":
-            // go left
-            break;
-        case "ArrowRight":
-            // go right
+            paddle2.velocity.y = speed;
             break;
     }
 })
