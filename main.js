583b070f3f4528f9242e81e64c2aa40a9463bf2e
raheemamer <raheem.amer22@gmail.com>
2022-10-01 02:03:26 +0200

    restrict paddle to boundaries

--- main.js
+++ main.js
@@ -17,6 +17,25 @@ class Paddle {
         c.fillStyle = "white";
         c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
+    update() {
+        this.draw();
+        if (this.position.y + this.velocity.y > 0 && this.position.y + this.height + this.velocity.y < canvas.height)
+            this.position.y += this.velocity.y;
+    }
+}
+
+class Ball {
+    constructor({ position }) {
+        this.position = position;
+        this.velocity = {
+            x: 1,
+            y: 1,
+        }
+    }
+    draw() {
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    }
 }
 
 const Paddle1 = new Paddle({
@@ -32,9 +51,52 @@ const Paddle2 = new Paddle({
         y: 100,
     },
 })
-Paddle1.draw()
-Paddle2.draw()
 
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.fillStyle = "black";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    Paddle1.update();
+    Paddle2.update();
+}
+animate();
+
+addEventListener('keydown', (event) => {
+    const speed = 3;
+    switch (event.key) {
+        // Player One key configurations
+        case "w":
+            // go up
+            Paddle1.velocity.y = -speed;
+            break;
+        case "s":
+            // go down
+            Paddle1.velocity.y = speed;
+            break;
+        case "a":
+            // go left
+            break;
+        case "d":
+            // go right
+            break;
+        // Player two key configurations
+        case "ArrowUp":
+            // go up
+            Paddle2.velocity.y = -speed;
+            break;
+        case "ArrowDown":
+            // go down
+            Paddle2.velocity.y = speed;
+            break;
+        case "ArrowLeft":
+            // go left
+            break;
+        case "ArrowRight":
+            // go right
+            break;
+    }
+})
 
 c.fillStyle = 'red';
 c.fillRect(0, 0, 100, 100);
